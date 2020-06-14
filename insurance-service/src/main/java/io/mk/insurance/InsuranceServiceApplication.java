aebd4639c70c6eca709e90fec693691f9ea3484c
Manoj Kumar <a.m.manojkumar@gmail.com>
2020-06-14 16:48:48 +0530

    fallback added

--- insurance-service/src/main/java/io/mk/insurance/InsuranceServiceApplication.java
+++ insurance-service/src/main/java/io/mk/insurance/InsuranceServiceApplication.java
@@ -2,6 +2,7 @@ package io.mk.insurance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -13,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
  */
 @SpringBootApplication
 @EnableFeignClients
+@EnableHystrix
 public class InsuranceServiceApplication {
 
 	public static void main(String[] args) {
--- insurance-service/src/main/java/io/mk/insurance/service/InsuranceService.java
+++ insurance-service/src/main/java/io/mk/insurance/service/InsuranceService.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 import io.mk.insurance.feign.client.InsuranceClient;
 import io.mk.insurance.model.InsuranceDetail;
 import io.mk.insurance.model.Insurer;
@@ -19,13 +21,14 @@ public class InsuranceService {
 
 	@Autowired
 	private InsuranceClient insuranceClient;
-	
+
 	@Value("${abc.insurance.app.name}")
 	String abcAppName;
-	
+
 	@Value("${xyz.insurance.app.name}")
 	String xyzAppName;
 
+	@HystrixCommand(fallbackMethod = "getDataFallBack")
 	public InsuranceDetail getInsuranceDetail(String brand, String model) {
 		InsuranceDetail insuranceDetail = new InsuranceDetail();
 
@@ -35,5 +38,9 @@ public class InsuranceService {
 		insuranceDetail.getInsurers().add(insurerAbc);
 		insuranceDetail.getInsurers().add(insurerXyz);
 		return insuranceDetail;
+	}
+
+	public InsuranceDetail getDataFallBack(String brand, String model) {
+		return new InsuranceDetail();
 	}
 }
