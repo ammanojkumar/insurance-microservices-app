699bcd140c67d75f9b5e7735472610f18d7d8985
Manoj Kumar <a.m.manojkumar@gmail.com>
2020-07-29 09:49:38 +0530

    version change

--- insurance-service/src/main/java/io/mk/insurance/InsuranceServiceApplication.java
+++ insurance-service/src/main/java/io/mk/insurance/InsuranceServiceApplication.java
@@ -14,7 +14,7 @@ import org.springframework.web.client.RestTemplate;
  */
 @SpringBootApplication
 @EnableFeignClients
-@EnableHystrix
+//@EnableHystrix
 public class InsuranceServiceApplication {
 
 	public static void main(String[] args) {
